4d73d08f7bc31ecad08e4dd4a87ae43ee2296317
Charles Surett <surettcharles@gmail.com>
2016-06-08 13:36:44 -0400

    added new functions and more descriptions

--- apps.py
+++ apps.py
@@ -1,8 +1,10 @@
 from objc_util import ObjCClass, nsurl, ObjCInstance
+from datetime import datetime
 
 LSApplicationWorkspace = ObjCClass('LSApplicationWorkspace')
 workspace = LSApplicationWorkspace.defaultWorkspace()
 
+_timediff = 978307200
 
 class App (object):
     def __init__(self, app):
@@ -23,13 +25,37 @@ class App (object):
             bg += [str(b)]
         self.backgroundModes = bg
         self.vendor = str(self.app.vendorName())
-        self.ID = str(self.app.applicationIdentifier())
+        self.appID = str(self.app.applicationIdentifier())
+        if len(self.app.appTags()):
+            for i in app.appTags():
+                if i == 'hidden':
+                    self.hidden = True
+        else:
+            self.hidden = False
+        self.modTime = datetime.fromtimestamp(self.app.bundleModTime()+_timediff)
+        dfam = []
+        try:
+            for i in self.app.deviceFamily():
+                dfam += [i]
+            if 1 in dfam:
+                self.iPhoneUI = True
+            else:
+                self.iPhoneUI = False
+            if 2 in dfam:
+                self.iPadUI = True
+            else:
+                self.iPadUI = False
+        except:
+            pass
+        self.signerID = str(self.app.signerIdentity())
+        self.adHoc = self.app.isAdHocCodeSigned()
+        self.sdkVersion = str(self.app.sdkVersion())
 
     def __str__(self):
-        return self.ID
+        return self.appID
 
     def __repr__(self):
-        return self.ID
+        return self.appID
 
 
 def _objcDict(objcd):
@@ -37,6 +63,7 @@ def _objcDict(objcd):
     for i in zip(objcd.allKeys(), objcd.allValues()):
         returns += [{i[0]: i[1]}]
     return returns
+
 
 def _urlHandle(url):
     if type(url) != ObjCInstance:
@@ -102,3 +129,24 @@ def audioComponentApps():
 def overideChecker(url):
     url=_urlHandle(url)
     return workspace.URLOverrideForURL_(url)
+
+
+def getVendors(applist=allApps()):
+    vendors = []
+    for i in applist:
+        if i.vendor not in vendors:
+            vendors += [i.vendor]
+    return vendors
+
+
+
+'''def search(app_list=allApps(), beta=None, redownload=None, app_type=None, fileSharing=None,
+           settingsBundle=None, vendor=None):
+    matches = []
+    for i in app_list:
+        if beta != None:
+            if i.beta == beta:
+'''
+
+
+
