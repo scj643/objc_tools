515e940720e6c017f6dd9aeab6d5058ebb30078d
Charles Surett <surettcharles@gmail.com>
2016-06-14 08:56:18 -0400

    made device.py useful

--- device.py
+++ device.py
@@ -1,2 +1,27 @@
 from objc_util import ObjCClass
 UIDevice = ObjCClass('UIDevice')
+NSProcessInfo = ObjCClass('NSProcessInfo')
+device = UIDevice.currentDevice()
+process = NSProcessInfo.processInfo()
+
+def lowPowerModeStatus():
+    return process.isLowPowerModeEnabled()
+    
+
+def hostName():
+    return str(process.hostName())
+    
+
+def osVersion():
+    major = process.operatingSystemVersion().a
+    minor = process.operatingSystemVersion().b
+    patch = process.operatingSystemVersion().c
+    return (major, minor, patch)
+    
+
+def deviceName():
+    return str(device.name())
+    
+
+def deviceType():
+    return str(device.model())
