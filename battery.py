692c34c669f2f61c7cf7da99f2f23c49901b3394
Charles Surett <surettcharles@gmail.com>
2016-06-29 12:12:07 -0400

    made battery.py usefull and added functions to device.py

--- battery.py
+++ battery.py
@@ -1,12 +1,32 @@
 from objc_util import ObjCClass
 
+__all__ = ['batter','backlight_level','set_backlight_level']
 UIDevice = ObjCClass('UIDevice')
 device = UIDevice.currentDevice()
 battery_states = {1: 'unplugged', 2: 'charging', 3: 'full'}
 
 class battery(object):
-    __init__(self):
+
+    def __init__(self):
         device.setBatteryMonitoringEnabled(True)
-    
-    def charging(self):
         
+    def __enter__(self):
+        device.setBatteryMonitoringEnabled(True)
+        return self
+    
+    def charge_status(self):
+        return battery_states[device.batteryState()]
+    
+    def level(self):
+        return device.batteryLevel()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        device.setBatteryMonitoringEnabled(False)
+
+def backlight_level():
+    return device._backlightLevel()
+
+def set_backlight_level(value):
+    if type(value) == float:
+        if 0<= value <= 1:
+            device._setBacklightLevel_(value)
--- device.py
+++ device.py
@@ -3,6 +3,7 @@ UIDevice = ObjCClass('UIDevice')
 NSProcessInfo = ObjCClass('NSProcessInfo')
 device = UIDevice.currentDevice()
 process = NSProcessInfo.processInfo()
+_fsizes = {'B': 1.0, 'KB': 1024.0, 'MB':  float(pow(1024,2)), 'GB': float(pow(1024,3))}
 
 def lowPowerModeStatus():
     return process.isLowPowerModeEnabled()
@@ -17,11 +18,58 @@ def osVersion():
     minor = process.operatingSystemVersion().b
     patch = process.operatingSystemVersion().c
     return (major, minor, patch)
-    
+
+
+def osVersionString():
+    return str(process.activeProcessorCount())
+
 
 def deviceName():
     return str(device.name())
-    
+
 
 def deviceType():
     return str(device.model())
+
+
+def deviceGeneration():
+    return str(device._currentProduct())
+
+    
+def activeProcessors():
+    return process.activeProcessorCount()
+
+
+def physicalMemory(unit='MB'):
+    if unit in _fsizes:
+        divider = _fsizes[unit]
+    else:
+        divider = _fsizes['MB']
+    mem = int(process.physicalMemory())
+    return mem/float(divider)
+
+
+def processorCount():
+    return process.processorCount()
+
+
+def setProcessName(name):
+    if type(name) == str:
+        process.setProcessName_(name)
+        return True
+    else:
+        return False
+
+
+def processName():
+    return str(process.processName())
+    
+
+def systemUptime():
+    '''
+    Returns system uptime in seconds
+    '''
+    return process.systemUptime()
+
+
+
